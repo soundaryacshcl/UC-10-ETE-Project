2a276b352cb95611a86d877f340db191f797baa8
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-29 13:22:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "mybucket-name-usecase-10"
+    bucket       = "mybucket-name-uc-10"
     key          = "usecase10/statefile.tfstate"
     region       = "us-east-1"
     encrypt      = true
